ec6c8968732e9e0b29040ea95048c186583a75a9
gaopeiru <ac1bca2@gmail.com>
2021-01-28 11:56:05 +0800

    更新

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import cv2
 import numpy as np
-img = cv2.imread('14545645.jpg',1)
+from matplotlib import pyplot as plt
+img = cv2.imread('14545645.jpg',0)
 print(img)
-cv2.namedWindow('14545645',cv2.WINDOW_NORMAL)
-cv2.imshow('test',img)
-cv2.imwrite('158.jpg',img)
+plt.imshow(img,cmap='gray', interpolation='bicubic')
+plt.xticks([]),plt.yticks([])
+plt.show()
+#cv2.namedWindow('14545645',cv2.WINDOW_NORMAL)
+#cv2.imshow('test',img)
+#cv2.imwrite('158.jpg',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
