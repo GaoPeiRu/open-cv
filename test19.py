bdd07c124d2b08234f5bfa5f50fa61933f495614
gaopeiru <ac1bca2@gmail.com>
2021-01-29 17:03:07 +0800

    更新

--- test19.py
+++ test19.py
@@ -1,35 +1,23 @@
 import cv2
 import numpy as np
-
-
 def nothing(x):
     pass
 
-
-img = np.zeros((600, 800, 3), np.uint8)
+img = cv2.imread('test13.jpg', 0)
+cv2.imshow('image13', img)
+edges = cv2.Canny(img, 50, 100)
 cv2.namedWindow('image')
+cv2.createTrackbar('minval', 'image', 0, 255, nothing)
+cv2.createTrackbar('maxval', 'image', 0, 255, nothing)
 
-cv2.createTrackbar('R', 'image', 0, 255, nothing)
-cv2.createTrackbar('G', 'image', 0, 255, nothing)
-cv2.createTrackbar('B', 'image', 0, 255, nothing)
-
-switch = '0:OFF\n1:ON'
-cv2.createTrackbar(switch, 'image', 0, 1, nothing)
 while(1):
-    cv2.imshow('image', img)
+    cv2.imshow('image', edges)
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
-
-    r = cv2.getTrackbarPos('R', 'image')
-    g = cv2.getTrackbarPos('G', 'image')
-    b = cv2.getTrackbarPos('B', 'image')
-    s = cv2.getTrackbarPos(switch, 'image')
-
-    if s == 0:
-        img[:] = 0
-    else:
-        img[:] = [b, g, r]
+    minval = cv2.getTrackbarPos('minval', 'image')
+    maxval = cv2.getTrackbarPos('maxval', 'image')
+    edges = cv2.Canny(img, minval, maxval)
 
 cv2.destroyAllWindows()
 
