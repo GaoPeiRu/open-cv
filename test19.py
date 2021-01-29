b54c3e941664c5c55cf7c0a4fba398358705b165
gaopeiru <ac1bca2@gmail.com>
2021-01-29 16:59:21 +0800

    更新

---
+++ test19.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+
+
+def nothing(x):
+    pass
+
+
+img = np.zeros((600, 800, 3), np.uint8)
+cv2.namedWindow('image')
+
+cv2.createTrackbar('R', 'image', 0, 255, nothing)
+cv2.createTrackbar('G', 'image', 0, 255, nothing)
+cv2.createTrackbar('B', 'image', 0, 255, nothing)
+
+switch = '0:OFF\n1:ON'
+cv2.createTrackbar(switch, 'image', 0, 1, nothing)
+while(1):
+    cv2.imshow('image', img)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+    r = cv2.getTrackbarPos('R', 'image')
+    g = cv2.getTrackbarPos('G', 'image')
+    b = cv2.getTrackbarPos('B', 'image')
+    s = cv2.getTrackbarPos(switch, 'image')
+
+    if s == 0:
+        img[:] = 0
+    else:
+        img[:] = [b, g, r]
+
+cv2.destroyAllWindows()
+
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('test6.jpg', 0)
+edges = cv2.Canny(img, 50, 100)
+edges2 = cv2.Canny(img, 50, 150)
+edges3 = cv2.Canny(img, 50, 200)
+edges4 = cv2.Canny(img, 100, 150)
+edges5 = cv2.Canny(img, 200, 250)
